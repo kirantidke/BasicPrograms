43cd9f81e1a910f9d522f46c248bac16f30a147c
kirantidke <kirantidke2013@.com>
2021-06-17 17:19:21 +0530

    commited even odd to new branch

---
+++ src/Bridgelabz/EvenOdd.java
@@ -0,0 +1,20 @@
+package Bridgelabz;
+
+import java.util.Scanner;
+
+public class EvenOdd {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		System.out.print("enter the number = ");
+		int number = s.nextInt();
+		if(number % 2 == 0)
+		{
+			System.out.println("number is even ");
+		}
+		else
+			System.out.println("number is odd");
+		
+	}
+
+}
